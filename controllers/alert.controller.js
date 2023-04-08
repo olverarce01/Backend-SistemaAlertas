0e26316df90ef8d0da2dae86ac8392778a06d2c9
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-04-08 09:50:43 -0400

    update admin one initialize

--- controllers/alert.controller.js
+++ controllers/alert.controller.js
@@ -4,8 +4,7 @@ import Group from '../models/Group.model.js';
 import User from '../models/User.model.js';
 import UserGroup from '../models/UserGroup.model.js';
 import mongoose from 'mongoose';
-import admin from 'firebase-admin';
-
+import admin from '../admin.js';
 
 const getMyAlerts = async (req,res)=>{
   if(!req.user){
@@ -62,20 +61,6 @@ const postAlert = async (req,res) => {
     });
     alert.save();
 
-    admin.initializeApp({
-      credential: admin.credential.cert({        
-        type: "service_account",
-        project_id: "vigilant-12535",
-        private_key_id: process.env.Private_key_id,
-        private_key: process.env.Private_key ? JSON.parse(process.env.Private_key) : undefined,
-        client_email: "firebase-adminsdk-v6l96@vigilant-12535.iam.gserviceaccount.com",
-        client_id: "106643052260083512683",
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://oauth2.googleapis.com/token",
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-v6l96%40vigilant-12535.iam.gserviceaccount.com"
-      })
-    });
     try {
       await admin.messaging().sendMulticast({
         tokens:tokens,
