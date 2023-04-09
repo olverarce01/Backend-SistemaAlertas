0aa3590edee1509afda0f525034135fc4e4f202f
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-04-09 09:11:58 -0400

    update version importscript

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,8 @@
 // import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 // import { getMessaging, onMessage } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-messaging.js";
 
-importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-messaging.js');
-
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 // Inicializa Firebase
 var firebaseConfig = {
