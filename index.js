0c0ac857edf3a3a7e1472f852b30919ac023c1a9
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-04-09 16:50:02 -0400

    remove test frontend

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
-app.get('/', function(req,res){res.render('index')})
+// app.get('/', function(req,res){res.render('index')})
 
 
 app.get('/error', function(req,res){res.json({message: "error de login"});});
