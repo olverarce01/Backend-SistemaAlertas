a92e322456eaaa4ee0763939cf2eabfa4bcd40ee
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-04-16 10:21:53 -0400

    remove view public files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import ejs from 'ejs'
+// import ejs from 'ejs'
 import express from "express";
 import cors from 'cors';
 import swaggerJsdoc from 'swagger-jsdoc';
@@ -36,8 +36,8 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+// app.set('view engine', 'ejs');
 
 /** 
  * @swagger
